3e8d28d2d324f7c054d86b1b6cb096b05122b6f1
Marco Pashkov <marcopashkov@gmail.com>
2014-03-30 17:03:43 -0700

    code can be generically executed; added pseudo-hidden URL-indicators

--- pysheet/server.py
+++ pysheet/server.py
@@ -37,41 +37,43 @@ def list_tables(name):
     tables = [t.serialized for t in Project.get(name=name).tables]
     return return_json( tables )
 
-@app.route('/project/<project>/create_table/<table>', methods=['POST'])
+@app.route('/project/<project>/_create_table/<table>', methods=['POST'])
 def create_table(project, table):
     table = Table.create_new_table(package_name=project, table_name=table)
     return return_json( table )
 
-@app.route('/project/<project>/<table>/source/<method>')
+@app.route('/project/<project>/<table>/_source/<method>')
 def get_table_method_source(project, table, method):
     table = Project.get(name=project).get_table(name=table)
     return return_data( data=table().get_source(method=method), 
                         mimetype='text/x-script.phyton')
 
-@app.route('/project/<project>/<table>/update_source/<method>', methods=['POST'])
+@app.route('/project/<project>/<table>/_update_source/<method>', methods=['POST'])
 def update_table_method_source(project, table, method):
     table = Project.get(name=project).get_table(name=table)
     new_source = flask.request.data
     table().update_source(new_source=new_source, method=method)
     return 'ok'
 
-@app.route('/project/<project>/<table>/source')
+@app.route('/project/<project>/<table>/_source')
 def get_table_source(project, table):
     table = Project.get(name=project).get_table(name=table)
     return return_data( data=table().get_source(), 
                         mimetype='text/x-script.phyton')
 
-@app.route('/project/<project>/<table>/update_source', methods=['POST'])
+@app.route('/project/<project>/<table>/_update_source', methods=['POST'])
 def update_table_source(project, table):
     table = Project.get(name=project).get_table(name=table)
     new_source = flask.request.data
     table().update_source(new_source=new_source)
     return 'ok'
 
-@app.route('/project/<project>/<table>')
-def run_table(project, table):
+@app.route('/project/<project>/<table>/<method>')
+def execute_table_method(project, table, method):
     table = Project.get(name=project).get_table(name=table)
-    return return_json( table().run() )
+    table = table()
+    func = getattr(table, method)
+    return return_json( func() )
 
 
 if __name__ == "__main__":
